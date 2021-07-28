f8bb5ad36b419627c8ee125247b979e0ed5273c9
hariz <harizanchal@gmail.com>
2021-07-28 23:39:07 +0530

    ss

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 const selectedAll = document.querySelectorAll(".selected");
+const select_box = document.querySelectorAll(".select-box");
 
 selectedAll.forEach((selected) => {
   const optionsContainer = selected.previousElementSibling;
@@ -9,6 +10,7 @@ selectedAll.forEach((selected) => {
   selected.addEventListener("click", () => {
     if (optionsContainer.classList.contains("active")) {
       optionsContainer.classList.remove("active");
+      
     } else {
       let currentActive = document.querySelector(".options-container.active");
 
@@ -18,15 +20,47 @@ selectedAll.forEach((selected) => {
 
       optionsContainer.classList.add("active");
     }
+    
   });
 
   optionsList.forEach((o) => {
     o.addEventListener("click", () => {
-      selected.innerHTML = o.querySelector("label").innerHTML;
+      // selected.innerHTML = o.querySelector("label").innerHTML;
+      selected.value =o.querySelector("label").innerHTML;
       optionsContainer.classList.remove("active");
+      alert("click");
+
     });
   });
   
 
   
+});
+
+
+// adapted from https://www.codeply.com/go/mhkMGnGgZo/bootstrap-4-validation-example
+
+$("#bootstrapForm").submit(function(event) {
+
+    // make selected form variable
+    var vForm = $(this);
+    
+  /*
+  If not valid prevent form submit
+  https://developer.mozilla.org/en-US/docs/Web/API/HTMLSelectElement/checkValidity
+  */
+    if (vForm[0].checkValidity() === false) {
+      event.preventDefault()
+      event.stopPropagation()
+    } else {
+     
+      // Replace alert with ajax submit here...
+      alert("your form is valid and ready to send");
+      
+    }
+    
+    // Add bootstrap 4 was-validated classes to trigger validation messages
+    vForm.addClass('was-validated');
+    
+ 
 });
